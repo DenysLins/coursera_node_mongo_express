89108b95d03dc9c4bc52d9e097f1645746605b68
Denys Lins <denyslins@gmail.com>
2018-12-24 00:03:41 -0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2240013249573035',
+        clientSecret: 'a1401cd9d2ea2c0ac69b09a43b3fa1a4'
+    }
 }
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -6,20 +6,22 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 router.use(bodyParser.json());
 
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
-    User.find({})
-        .then((users) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(users);
-        }, (err) => next(err))
-        .catch((err) => next(err));
-});
+router.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+    .get('/', cors.cors, authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+        User.find({})
+            .then((users) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(users);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
     User.register(new User({ username: req.body.username }),
         req.body.password, (err, user) => {
             if (err) {
@@ -49,14 +51,14 @@ router.post('/signup', (req, res, next) => {
         });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
     var token = authenticate.getToken({ _id: req.user._id });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
-router.get('/logout', (req, res) => {
+router.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); }).get('/logout', (req, res) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
@@ -66,6 +68,15 @@ router.get('/logout', (req, res) => {
         var err = new Error('You are not logged in!');
         err.status = 403;
         next(err);
+    }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        var token = authenticate.getToken({ _id: req.user._id });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ success: true, token: token, status: 'You are successfully logged in!' });
     }
 });
 
